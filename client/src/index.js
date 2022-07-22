64f329e6e30c451219dadafb9bfc024e623e98b2
r134x7 <r134x7@gmx.com>
2022-07-22 23:44:39 +1000

    setting up store and provider inside index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';q
+import store from "./utils/store";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
