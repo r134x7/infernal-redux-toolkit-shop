eed01771f66a8c8eb67502870108398af5f5b41d
r134x7 <r134x7@gmx.com>
2022-07-24 01:38:24 +1000

    added cartReducer, useSelector, useDispatch, refactored code

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,18 +1,25 @@
 import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { loadStripe } from '@stripe/stripe-js';
 import { useLazyQuery } from '@apollo/client';
 import { QUERY_CHECKOUT } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
-import { useStoreContext } from '../../utils/GlobalState';
-import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
+// import { useStoreContext } from '../../utils/GlobalState';
+// import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
+import { useSelector, useDispatch } from 'react-redux';
+import cartReducer, { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../features/cart";
 import './style.css';
 // check this file
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
-  const [state, dispatch] = useStoreContext();
+  // const [state, dispatch] = useStoreContext();
+
+  const cart = useSelector(state => state.cart)
+  const dispatch = useDispatch();
+
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
@@ -26,7 +33,10 @@ const Cart = () => {
   useEffect(() => {
     async function getCart() {
       const cart = await idbPromise('cart', 'get');
-      dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+      // dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+      dispatch(cartReducer(cart, ADD_MULTIPLE_TO_CART({
+        products: [...cart]
+      })))
     }
 
     if (!state.cart.length) {
@@ -35,7 +45,8 @@ const Cart = () => {
   }, [state.cart.length, dispatch]);
 
   function toggleCart() {
-    dispatch({ type: TOGGLE_CART });
+    // dispatch({ type: TOGGLE_CART });
+    dispatch(cartReducer(cart, TOGGLE_CART()));
   }
 
   function calculateTotal() {
