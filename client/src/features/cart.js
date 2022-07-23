f7bf945e1f1d6460160b84064856a0598f51aee2
r134x7 <r134x7@gmx.com>
2022-07-23 17:40:46 +1000

    updated cart createSlice

--- client/src/features/cart.js
+++ client/src/features/cart.js
@@ -2,19 +2,62 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = [];
 
-const productsSlice = createSlice({
-    name: "products",
+const cartSlice = createSlice({
+    name: "cart",
     initialState,
     reducers: {
-        UPDATE_PRODUCTS(state, action) {
+        ADD_TO_CART(state, action) {
             return {
                 ...state,
-                products: [...action.products],
-            }
+                cartOpen: true,
+                cart: [...state.cart, action.product],
+            };
+        },
+        UPDATE_CART_QUANTITY(state, action) {
+            return {
+                ...state,
+                cartOpen: true,
+                cart: state.cart.map(product => {
+                  if (action._id === product._id) {
+                    product.purchaseQuantity = action.purchaseQuantity
+                  }
+                  return product
+                })
+              };
+        },
+        REMOVE_FROM_CART(state, action) {
+            let newState = state.cart.filter(product => {
+                return product._id !== action._id;
+              });
+        
+              return {
+                ...state,
+                cartOpen: newState.length > 0,
+                cart: newState
+              };
+        },
+        ADD_MULTIPLE_TO_CART(state, action) {
+            return {
+                ...state,
+                cart: [...state.cart, ...action.products],
+              };
+        },
+        CLEAR_CART(state, action) {
+            return {
+                ...state,
+                cartOpen: false,
+                cart: []
+            };
+        },
+        TOGGLE_CART(state, action) {
+            return {
+                ...state,
+                cartOpen: !state.cartOpen
+              };
         }
     }
 });
 
-export const { UPDATE_PRODUCTS } = productsSlice.actions
+export const { ADD_TO_CART, UPDATE_CART_QUANTITY, ADD_MULTIPLE_TO_CART, REMOVE_FROM_CART, CLEAR_CART, TOGGLE_CART } = cartSlice.actions;
 
-export default productsSlice.reducer
+export default cartSlice.reducer;
