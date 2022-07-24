de7c6c0b99f74820c5f972017dcf1030d03edb4e
r134x7 <r134x7@gmx.com>
2022-07-25 03:34:39 +1000

    added comments for wild card route

--- server/server.js
+++ server/server.js
@@ -14,28 +14,19 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
-// server.applyMiddleware({ app });
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve up static assets
-app.use('/images', express.static(path.join(__dirname, '../client/images')));
+app.use('/images', express.static(path.join(__dirname, '../client/public')));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
+app.get('*', (req, res) => { // wild card route: Quote from 21-MERN/12-Stu_MERN setup: "Since the React front-end application will handle its own routing, we set up a wildcard route on our server that will serve the front end whenever a request for a non-API route is received."" 
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
-// db.once('open', () => {
-//   app.listen(PORT, () => {
-//     console.log(`API server running on port ${PORT}!`);
-//     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-//   });
-// });
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start(); // await to start the server
